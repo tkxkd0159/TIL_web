a096b7ce451d90de0e9db649466cf0c86ab53b17
LJS_KU <tkxkd0159@gmail.com>
2020-12-01 16:47:32 +0900

    Modify button position and input form

--- nomad/todo/todo.js
+++ nomad/todo/todo.js
@@ -8,68 +8,18 @@ let todoList = [];
 const FINISH = "finish";
 let finishList = [];
 
-function idxArrange(array){
-    for(let idx=0; idx < array.length; idx++){
-        array[idx].id = idx+1;
-    }
-    return array;
+function idxArrange(array) {
+  for (let idx = 0; idx < array.length; idx++) {
+    array[idx].id = idx + 1;
+  }
+  return array;
 }
-function drawFinishFromTodo(text) {
+function drawFinish(obj, option="default") {
   const finishElement = document.createElement("li");
   const delBtn = document.createElement("button");
+  delBtn.className = "del-todo-button"
   const returnBtn = document.createElement("button");
-  const span = document.createElement("span");
-  const finishIndex = finishList.length;
-  delBtn.innerHTML = "❌";
-  returnBtn.innerHTML = "🔼";
-  span.innerHTML = text;
-
-  delBtn.addEventListener("click", (event) => {
-    const finishElement = event.target.parentNode;
-    finishListHTML.removeChild(finishElement);
-    const cleanFinish = finishList.filter((finish) => {
-      return finish.id !== parseInt(finishElement.id);
-    });
-    finishList = idxArrange(cleanFinish);
-    saveFinish();
-    const finishHTMLson = finishListHTML.querySelectorAll("li");
-    idxArrange(finishHTMLson);
-  });
-
-  returnBtn.addEventListener("click", (event) => {
-    const finishElement = event.target.parentNode;
-    finishListHTML.removeChild(finishElement);
-    const cleanFinish = finishList.filter((finish) => {
-      return finish.id !== parseInt(finishElement.id);
-    });
-    if (finishList !== []) {
-      const addTodo = finishList.filter((finish) => {
-        return finish.id === parseInt(finishElement.id);
-      });
-      addTodo[0].id = todoList.length + 1;
-      todoList.push(addTodo[0]);
-      saveTodo();
-      drawTodoFromFinish(addTodo[0].text);
-    }
-
-    finishList = idxArrange(cleanFinish);
-    const finishHTMLson = finishListHTML.querySelectorAll("li");
-    idxArrange(finishHTMLson);
-    saveTodo();
-    saveFinish();
-  });
-
-  finishElement.appendChild(span);
-  finishElement.appendChild(delBtn);
-  finishElement.appendChild(returnBtn);
-
-  finishElement.id = finishIndex;
-  finishListHTML.appendChild(finishElement);
-}
-function drawFinishFromLoad(obj) {
-  const finishElement = document.createElement("li");
-  const delBtn = document.createElement("button");
-  const returnBtn = document.createElement("button");
+  returnBtn.className = "return-todo-button"
   const span = document.createElement("span");
   delBtn.innerHTML = "❌";
   returnBtn.innerHTML = "🔼";
@@ -83,14 +33,16 @@ function drawFinishFromLoad(obj) {
     });
     finishList = idxArrange(cleanFinish);
     saveFinish();
+    const finishHTMLson = finishListHTML.querySelectorAll("li");
+    idxArrange(finishHTMLson);
   });
+
   returnBtn.addEventListener("click", (event) => {
     const finishElement = event.target.parentNode;
     finishListHTML.removeChild(finishElement);
     const cleanFinish = finishList.filter((finish) => {
       return finish.id !== parseInt(finishElement.id);
     });
-
     if (finishList !== []) {
       const addTodo = finishList.filter((finish) => {
         return finish.id === parseInt(finishElement.id);
@@ -98,7 +50,7 @@ function drawFinishFromLoad(obj) {
       addTodo[0].id = todoList.length + 1;
       todoList.push(addTodo[0]);
       saveTodo();
-      drawTodoFromFinish(addTodo[0].text);
+      drawTodo(addTodo[0]);
     }
 
     finishList = idxArrange(cleanFinish);
@@ -108,22 +60,32 @@ function drawFinishFromLoad(obj) {
     saveFinish();
   });
 
-  finishList.push(obj);
-  finishElement.appendChild(span);
-  finishElement.appendChild(delBtn);
+  if (option === "load"){
+    finishList.push(obj);
+  }
+
   finishElement.appendChild(returnBtn);
+  finishElement.appendChild(delBtn);
+  finishElement.appendChild(span);
   finishElement.id = obj.id;
   finishListHTML.appendChild(finishElement);
 }
-function drawTodoFromFinish(text) {
+
+
+function drawTodo(input, option="default") {
   const todoElement = document.createElement("li");
   const delBtn = document.createElement("button");
+  delBtn.className = "del-todo-button"
   const chkBtn = document.createElement("button");
+  chkBtn.className = "chk-todo-button"
   const span = document.createElement("span");
-  const todoIndex = todoList.length;
   delBtn.innerHTML = "❌";
   chkBtn.innerHTML = "✅";
-  span.innerHTML = text;
+  if (option === "submit") {
+    span.innerHTML = input;
+  } else {
+    span.innerHTML = input.text;
+  }
 
   delBtn.addEventListener("click", (event) => {
     const todoElement = event.target.parentNode;
@@ -132,59 +94,7 @@ function drawTodoFromFinish(text) {
       return todo.id !== parseInt(todoElement.id);
     });
     todoList = idxArrange(cleanTodo);
-    const todoHTMLson = todoListHTML.querySelectorAll("li")
-    idxArrange(todoHTMLson);
-    saveTodo();
-  });
-  chkBtn.addEventListener("click", (event) => {
-    const todoElement = event.target.parentNode;
-    todoListHTML.removeChild(todoElement);
-    const cleanTodo = todoList.filter((todo) => {
-      return todo.id !== parseInt(todoElement.id);
-    });
-
-    if (todoList !== []) {
-      const addFinish = todoList.filter((todo) => {
-        return todo.id === parseInt(todoElement.id);
-      });
-      addFinish[0].id = finishList.length + 1;
-      finishList.push(addFinish[0]);
-      saveFinish();
-      drawFinishFromTodo(addFinish[0].text);
-    }
-
-    todoList = idxArrange(cleanTodo);
-    const todoHTMLson = todoListHTML.querySelectorAll("li")
-    idxArrange(todoHTMLson);
-    saveTodo();
-    saveFinish();
-  });
-
-  todoElement.appendChild(span);
-  todoElement.appendChild(delBtn);
-  todoElement.appendChild(chkBtn);
-  todoElement.id = todoIndex;
-  todoListHTML.appendChild(todoElement);
-}
-function drawTodo(text) {
-  const todoElement = document.createElement("li");
-  const delBtn = document.createElement("button");
-  const chkBtn = document.createElement("button");
-  const span = document.createElement("span");
-
-  const todoIndex = todoList.length + 1;
-  delBtn.innerHTML = "❌";
-  chkBtn.innerHTML = "✅";
-  span.innerHTML = text;
-
-  delBtn.addEventListener("click", (event) => {
-    const todoElement = event.target.parentNode;
-    todoListHTML.removeChild(todoElement);
-    const cleanTodo = todoList.filter((todo) => {
-      return todo.id !== parseInt(todoElement.id);
-    });
-    todoList = idxArrange(cleanTodo);
-    const todoHTMLson = todoListHTML.querySelectorAll("li")
+    const todoHTMLson = todoListHTML.querySelectorAll("li");
     idxArrange(todoHTMLson);
     saveTodo();
   });
@@ -201,26 +111,39 @@ function drawTodo(text) {
       addFinish[0].id = finishList.length + 1;
       finishList.push(addFinish[0]);
       saveFinish();
-      drawFinishFromTodo(addFinish[0].text);
+      drawFinish(addFinish[0]);
     }
     todoList = idxArrange(cleanTodo);
-    const todoHTMLson = todoListHTML.querySelectorAll("li")
+    const todoHTMLson = todoListHTML.querySelectorAll("li");
     idxArrange(todoHTMLson);
     saveTodo();
     saveFinish();
   });
 
-  todoElement.appendChild(span);
-  todoElement.appendChild(delBtn);
+
+
+  if (option === "submit") {
+    const todoIndex = todoList.length + 1;
+    todoElement.id = todoIndex;
+
+    const todoObj = {
+      text: input,
+      id: todoIndex,
+    };
+    todoList.push(todoObj);
+
+  } else {
+    todoElement.id = input.id;
+    if(option ==="load"){
+      todoList.push(input);
+    }
+  }
   todoElement.appendChild(chkBtn);
-  todoElement.id = todoIndex;
+  todoElement.appendChild(delBtn);
+  todoElement.appendChild(span);
+
   todoListHTML.appendChild(todoElement);
 
-  const todoObj = {
-    text: text,
-    id: todoIndex,
-  };
-  todoList.push(todoObj);
   saveTodo();
 }
 
@@ -236,7 +159,7 @@ function loadTodo() {
   if (loadTodos !== null) {
     loadTodos = JSON.parse(loadTodos);
     loadTodos.forEach((todo) => {
-      drawTodo(todo.text);
+      drawTodo(todo, "load");
     });
   }
 }
@@ -247,7 +170,7 @@ function loadFinish() {
   if (loadFinishes !== null) {
     loadFinishes = JSON.parse(loadFinishes);
     loadFinishes.forEach((finish) => {
-      drawFinishFromLoad(finish);
+      drawFinish(finish, "load");
     });
   }
 }
@@ -258,7 +181,7 @@ function init() {
   todoForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const currentValue = todoInput.value;
-    drawTodo(currentValue);
+    drawTodo(currentValue, "submit");
     todoInput.value = "";
   });
 }
