3709cd415096a89f828767aba10cc5c1460dd569
Jaeseung Lee(Home) <41176085+tkxkd0159@users.noreply.github.com>
2021-03-10 19:46:06 +0900

    Practice recursive

--- js/test.js
+++ js/test.js
@@ -2,4 +2,5 @@ console.log(Number(true) == String(1))
 console.log('5'/true)
 
 const doubleArray = [2,3, [2,3]]
-console.log(doubleArray)
+console.log(!Array.isArray(doubleArray) || !true)
+console.log(typeof 1);
