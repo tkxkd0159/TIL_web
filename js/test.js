3b263ea78ae08949e46ea1a950747c2c4cd1da24
Jaeseung Lee(Home) <41176085+tkxkd0159@users.noreply.github.com>
2021-03-25 23:05:30 +0900

    npm init
    Set type: module

--- js/test.js
+++ js/test.js
@@ -1,6 +1,13 @@
+
+
 console.log(Number(true) == String(1))
 console.log('5'/true)
 
 const doubleArray = [2,3, [2,3]]
 console.log(!Array.isArray(doubleArray) || !true)
 console.log(typeof 1);
+
+import random from "random";
+
+
+console.log(random.boolean())
